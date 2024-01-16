bd1ee9e803e0430f9aa26f3ec4c000d5a13140f7
Faten Yahiaoui <yahiaouifaten@gmail.com>
2024-01-16 17:18:45 -0500

    Adding Initial Blog Structure

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
